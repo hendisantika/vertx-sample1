61cdd086a8affefbc8601c45cf3b6bbdfa110597
hendisantika <hendisantika@yahoo.co.id>
2025-01-10 06:06:03 +0700

    Add writeJsonResponse in ApiHelper🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/vertx_sample1/ApiHelper.java
+++ src/main/java/id/my/hendisantika/vertx_sample1/ApiHelper.java
@@ -1,5 +1,12 @@
 package id.my.hendisantika.vertx_sample1;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.RoutingContext;
+
+import java.util.NoSuchElementException;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : vertx-sample1
@@ -11,4 +18,19 @@ package id.my.hendisantika.vertx_sample1;
  * To change this template use File | Settings | File Templates.
  */
 public class ApiHelper {
+  private static <T> Handler<AsyncResult<T>> writeJsonResponse(RoutingContext context, int status) {
+    return ar -> {
+      if (ar.failed()) {
+        if (ar.cause() instanceof NoSuchElementException) {
+          context.response().setStatusCode(404).end(ar.cause().getMessage());
+        } else {
+          context.fail(ar.cause());
+        }
+      } else {
+        context.response().setStatusCode(status)
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .end(Json.encodePrettily(ar.result()));
+      }
+    };
+  }
 }
